2bd009d39ab6d368e83a77a62a9da06c1a2eeb2e
1cedsoda <29167146+1cedsoda@users.noreply.github.com>
2024-04-23 23:35:56 +0200

    Callout -> Admonitions

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,5 +1,6 @@
 import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
+import Admonition from "@theme/Admonition";
 import { Callout } from "../components/Callout";
 import { ImageFloat } from "../components/ImageFloat";
 import { YouTube } from "../components/YouTube";
@@ -13,7 +14,7 @@ import { TikTok } from "../components/TikTok";
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  // Map the "<Callout>" tag to our Callout component
+  // Map the "<Admonition>" tag to our Callout component
   // `Callout` will receive all props that were passed to `<Highlight>` in MDX
   Callout,
   ImageFloat,
@@ -24,4 +25,5 @@ export default {
   Button,
   Spotify,
   TikTok,
+  Admonition,
 };
