43cee52758322c968164735090fc1d0249a223e1
1cedsoda <29167146+1cedsoda@users.noreply.github.com>
2024-04-23 22:22:49 +0200

    everything mdx + bug fixes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,9 +60,9 @@ const config: Config = {
       items: [
         {
           type: "docSidebar",
-          sidebarId: "tutorialSidebar",
+          sidebarId: "startupBasicsSidebar",
           position: "left",
-          label: "Roadmap",
+          label: "Startup Basics",
         },
         {
           href: githubUrl,
