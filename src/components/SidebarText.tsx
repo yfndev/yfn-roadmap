32693d3efe1f5a2d27dcd97afe8f735278d44c18
Joelheile <joel.heil-escobar@outlook.de>
2024-04-23 13:12:38 +0200

    feat: added all pages into custom sidebar

---
+++ src/components/SidebarText.tsx
@@ -0,0 +1,16 @@
+export const SidebarText = ({ children }) => (
+    <div
+      style={{
+        backgroundColor: "var(--background-light)",
+        display: "flex",
+        alignItems: "center",
+        alignContent: "center",
+        paddingTop: "1rem",
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+      }}
+    >
+      <div style={{ alignSelf: "center" }}>{children}</div>
+    </div>
+  );
+  
