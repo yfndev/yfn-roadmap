9433853320250a2a8cc2c28aeabcd592e1306c5d
Joelheile <joel.heil-escobar@outlook.de>
2024-04-24 19:22:14 +0200

    chore: format

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,6 @@ module.exports = {
               from: { height: "var(--radix-accordion-content-height)" },
               to: { height: 0 },
             },
-            
           },
           animation: {
             "accordion-down": "accordion-down 0.2s ease-out",
