b5c8cd2eec9a3e1472766c3b5940b230cda0ae98
Joelheile <joel.heil-escobar@outlook.de>
2024-04-28 19:00:58 +0200

    chore: format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
   content: ["./src/components/**/*.{html,js,jsx,ts,tsx}"],
   darkMode: ["class"],
   theme: {
-   
     fontFamily: {
       sans: ["Readex Pro", "sans-serif"],
     },
