b8d5efe07e72dcf234304c80470e955b2e3e1f8f
Luca Kammerer <lucakammerer2@gmail.com>
2026-04-10 21:25:23 +0200

    update default url to yfn-roadmap-ten.vercel.app

--- src/constants.ts
+++ src/constants.ts
@@ -7,9 +7,9 @@ function getEnv(key: string): string | undefined {
   return value;
 }
 
-const url = getEnv("URL") ?? "https://youngfounders.network";
+const url = getEnv("URL") ?? "https://yfn-roadmap-ten.vercel.app";
 const yfnUrl = getEnv("YFN_URL") ?? "https://youngfounders.network";
-const baseUrl = getEnv("BASE_URL") ?? "/roadmap";
+const baseUrl = getEnv("BASE_URL") ?? "/";
 const githubUrl = "https://github.com/yfndev/yfn-roadmap";
 
 export { url, yfnUrl, baseUrl, githubUrl };
